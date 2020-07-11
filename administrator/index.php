4053b7530500cb121b408f2124d15a8363bb6282
Bhavesh Patel <bhaveshpatel640@gmail.com>
2020-07-11 23:07:24 +0530

    About detials updated Changes http to https

--- administrator/index.php
+++ administrator/index.php
@@ -1,5 +1,5 @@
 <?php
 echo $_SERVER['REQUEST_URI'];
 if(strcmp($_SERVER['REQUEST_URI'],'/administrator/')==0){
-	header("Location: http://".$_SERVER['HTTP_HOST']);
+	header("Location: https://".$_SERVER['HTTP_HOST']);
 }
